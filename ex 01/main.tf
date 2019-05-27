b0558b38bfc0f3a94aef8fdef18b30a43adc021d
Marcio Godoi <souzagodoi@gmail.com>
2019-05-27 16:45:33 -0300

    Criação primeiro exercício

---
+++ ex 01/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "gcs" {
+    bucket = "juit-terraform-state"
+    prefix = "terraform/ex01"
+    credentials = "../../../credential/terraform-account.json"
+  }
+}
+
+resource "google_compute_instance" "ex01" {
+  name         = "ex01"
+  machine_type = "n1-standard-1"
+  project = "nome_projeto"
+  zone         = "nome_zona"
+
+    boot_disk {
+        initialize_params {
+        image = "cos-cloud/cos-stable-72-11316-136-0"
+        size    = "30"
+        type    = "pd-ssd"
+        }
+        auto_delete = true
+    }
+
+    tags = ["api", "prd"]
+}
